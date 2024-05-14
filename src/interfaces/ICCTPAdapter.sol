8bfa4183b977368c4f25e754a2035543e7c26159
foxytanuki <biz@foxytanuki.dev>
2024-05-14 20:35:44 +0900

    Add CCTP Interface drafts

---
+++ src/interfaces/ICCTPAdapter.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
+
+interface ICCTPAdapter {
+    function cctpSend(
+        address sender,
+        uint256 dstChainId,
+        address recipient,
+        bytes calldata message,
+        uint256 fee,
+        bytes calldata params
+    )
+        external
+        payable;
+
+    function estimateFee(
+        address sender,
+        uint256 dstChainId,
+        address recipient,
+        address asset,
+        bytes calldata message,
+        uint256 amount,
+        bytes calldata params
+    )
+        external
+        view
+        returns (uint256);
+}
