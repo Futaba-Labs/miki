79d0f39833db4c4e20003be68540cf34ba3999d5
adachi_440 <tomoki1999314@gmail.com>
2024-06-15 23:44:17 +0800

    add bnb testnet

--- bin/deploy_spoke_chain.sh
+++ bin/deploy_spoke_chain.sh
@@ -4,43 +4,43 @@ source ./.env
 
 array=("$@")
 
-# echo "Deploying spoke chains..."
+echo "Deploying spoke chains..."
 
-# for item in "${array[@]}"
-# do
-#   echo Chain: ${item}
-#   if [ "${item}" = "mantle_sepolia" ] || [ "${item}" = "astar_zkyoto" ] || [ "${item}" = "polygon_cardona" ]; then
-#     echo "Deploying with legacy..."
-#     if [ "${item}" = "mantle_sepolia" ]; then
-#       forge script script/Deploy.s.sol --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY} --legacy -g 4000000
-#     else
-#       forge script script/Deploy.s.sol --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY} --legacy --with-gas-price 3000000000
-#     fi
-#   else
-#     forge script script/Deploy.s.sol --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
-#   fi
-# done
+for item in "${array[@]}"
+do
+  echo Chain: ${item}
+  if [ "${item}" = "mantle_sepolia" ] || [ "${item}" = "astar_zkyoto" ] || [ "${item}" = "polygon_cardona" ]; then
+    echo "Deploying with legacy..."
+    if [ "${item}" = "mantle_sepolia" ]; then
+      forge script script/Deploy.s.sol --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY} --legacy -g 4000000
+    else
+      forge script script/Deploy.s.sol --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY} --legacy --with-gas-price 3000000000
+    fi
+  else
+    forge script script/Deploy.s.sol --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
+  fi
+done
 
-# echo Setting LayerZero/Axelar Peer...
+echo Setting LayerZero/Axelar Peer...
 
-# echo Chain: arbitrum_sepolia
+echo Chain: arbitrum_sepolia
 
-# forge script script/adapters/SetPeer.s.sol -s "setLzAdapterPeer()" --rpc-url arbitrum_sepolia --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
+forge script script/adapters/SetPeer.s.sol -s "setLzAdapterPeer()" --rpc-url arbitrum_sepolia --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
 
-# for item in "${array[@]}"
-# do
-#   echo Chain: ${item}
+for item in "${array[@]}"
+do
+  echo Chain: ${item}
 
-#   if [ "${item}" = "mantle_sepolia" ]; then
-#     forge script script/adapters/SetAxelarReceiver.s.sol --rpc-url arbitrum_sepolia --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
-#   else
-#     if [ "${item}" = "astar_zkyoto" ] || [ "${item}" = "polygon_cardona" ]; then
-#       forge script script/adapters/SetPeer.s.sol -s "setLzAdapterPeer()" --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY} --legacy
-#     else
-#       forge script script/adapters/SetPeer.s.sol -s "setLzAdapterPeer()" --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
-#     fi
-#   fi
-# done
+  if [ "${item}" = "mantle_sepolia" ]; then
+    forge script script/adapters/SetAxelarReceiver.s.sol --rpc-url arbitrum_sepolia --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
+  else
+    if [ "${item}" = "astar_zkyoto" ] || [ "${item}" = "polygon_cardona" ]; then
+      forge script script/adapters/SetPeer.s.sol -s "setLzAdapterPeer()" --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY} --legacy
+    else
+      forge script script/adapters/SetPeer.s.sol -s "setLzAdapterPeer()" --rpc-url $item --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
+    fi
+  fi
+done
 
 echo Setting Bridge Adapters...
 
@@ -50,6 +50,6 @@ do
   forge script script/Deploy.s.sol -s "setBridgeAdapter(string, address)" $item 0x0000000000000000000000000000000000000000 --rpc-url arbitrum_sepolia --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
 done
 
-# echo Setting LayerZero eids...
+echo Setting LayerZero eids...
 
-# forge script script/Deploy.s.sol -s "setEids()" --rpc-url arbitrum_sepolia --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
+forge script script/Deploy.s.sol -s "setEids()" --rpc-url arbitrum_sepolia --broadcast --verify -vvvv --via-ir --ffi --private-key ${PRIVATE_KEY}
