7f73d572200d31f2c004eefdcc24e8524e1f3b34
foxytanuki <biz@foxytanuki.dev>
2024-07-09 04:50:12 +0900

    Update constants of relayers/gelato-cctp

--- src/relayers/gelato-cctp/constants.ts
+++ src/relayers/gelato-cctp/constants.ts
@@ -2,36 +2,36 @@ export const TWO_HOURS = 2 * 60 * 60 * 1000;
 
 interface INetwork {
   domain: number;
-  gelatoCCTPSender: string;
-  gelatoCCTPReceiver: string;
+  mikiCCTPAdapter: string;
+  mikiCCTPReceiver: string;
   circleTokenMessenger: string;
   circleMessageTransmitter: string;
   usdc: string;
 }
 
 export enum ChainId {
-  Avalanche = 43114,
-  Arbitrum = 42161,
+  Arbitrum = 421614, // Arbitrum Sepolia
+  Base = 84532, // Base Sepolia
 }
 
 export const NETWORKS: { [id in ChainId]: INetwork } = {
-  [ChainId.Avalanche]: {
-    domain: 1,
-    gelatoCCTPSender: "0x9b841affdd62404A7Fd1ecc384103c35CFBaeb17",
-    gelatoCCTPReceiver: "0xec88F7f07bb838AC3A336C1C2868b89bE2E90a1D",
-    circleTokenMessenger: "0x6b25532e1060ce10cc3b0a99e5683b91bfde6982",
-    circleMessageTransmitter: "0x8186359af5f57fbb40c6b14a588d2a59c0c29880",
-    usdc: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
-  },
   [ChainId.Arbitrum]: {
     domain: 3,
-    gelatoCCTPSender: "0xD8279B27f574dEfA6b58A86388D712653DAc416b",
-    gelatoCCTPReceiver: "0x290138D33Fb86d0E8484BF7C4b3952277d5fE4c6",
-    circleTokenMessenger: "0x19330d10D9Cc8751218eaf51E8885D058642E08A",
-    circleMessageTransmitter: "0xC30362313FBBA5cf9163F0bb16a0e01f01A896ca",
-    usdc: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+    mikiCCTPAdapter: "0x330F25c20621dE38132516dcC9C7C49982B37A23",
+    mikiCCTPReceiver: "",
+    circleTokenMessenger: "0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5",
+    circleMessageTransmitter: "0xaCF1ceeF35caAc005e15888dDb8A3515C41B4872",
+    usdc: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d",
+  },
+  [ChainId.Base]: {
+    domain: 6,
+    mikiCCTPAdapter: "",
+    mikiCCTPReceiver: "0x1d3E172C336D782B7d3869A9541b039DA376AA5C",
+    circleTokenMessenger: "0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5",
+    circleMessageTransmitter: "0x7865fAfC2db2093669d92c0F33AeEF291086BEFD",
+    usdc: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
   },
 };
 
 export const GELATO_API = "https://api.gelato.digital";
-export const CIRCLE_API = "https://iris-api.circle.com";
+export const CIRCLE_API = "https://iris-api-sandbox.circle.com";
